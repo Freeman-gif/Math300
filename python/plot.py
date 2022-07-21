5030a3fbe22bb8ad4babff71e876471e59cbced3
Freeman-gif <freemanchen115@gmail.com>
2022-07-20 19:18:27 -0700

    push commit

---
+++ python/plot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+from test import circle
+import random
+
+circle = circle(cx = random.randint(1,25),cy = random.randint(1,25),r = random.randint(1,10))
--- python/test.py
+++ python/test.py
@@ -1,8 +1,13 @@
 from re import X
+from telnetlib import theNULL
 import numpy as np
 import pandas as pd
 import math
+from regex import R
+import sympy as sp
+from IPython.display import display
 
+import matplotlib.pyplot as plt
 def distance(x1,y1,x2,y2):
     dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return dist
@@ -24,3 +29,87 @@ people = [person for people in range(pop)]
 
 cub = lambda x: x*x*x
 print(cub(2))
+
+def trapezoid(f,a=0,b=1,n=100):
+    summation = 0
+    h = (b - a)/n
+    if n <= 0:
+        raise ValueError('The fuck, n is not positive' )
+    else:
+        for i in range(n-1):
+            summation += h * i
+    return h/2(f(a)+f(b)+2*((summation)))
+
+        
+
+
+
+sp.init_printing
+
+x,y = sp.symbols('x,y')
+
+expr = (x+y)**4
+
+display(expr.expand())
+
+def f(x): 
+    return math.sqrt(x)
+functionf = f
+
+def calculate(f,a = 0,b = 1,n = 60 ):
+    summation = 0
+    g = 0
+    listx = []
+    listy = []
+    x = (a + abs(a - b))/n
+    k = abs(a-b)
+    if n <= 0:
+        raise ValueError('The fuck, n is not positive' )
+    else :
+        for i in range(n-1):
+            g += (a + abs(a - b))/n
+            listx.append(g)
+            listy.append(f(g))
+            summation += f(x) * k
+        width = (a+abs(a - b))/n
+        # plt.bar(listx,listy,width = width,alpha=0.2,align='edge',edgecolor='b')
+        # plt.plot(listx,listy)
+        # plt.xlabel("X",fontsize='13')	#adds a label in the x axis
+        # plt.ylabel("Y",fontsize='13')	#adds a label in the y axis
+        # plt.legend(('YvsX'),loc='best')	#creates a legend to identify the plot
+        # plt.show()
+    return print(summation)
+
+
+calculate(functionf,10,40,15)
+
+
+
+
+class ellipse:
+    def __init__(self, cx = 0  ,cy = 0, r = 0):
+        self.cx = cx
+        self.r = r
+        self.cy = cy
+
+class circle(ellipse):
+    def __init__(self,cx=0, cy = 0, r=0):
+        super().__init__(cx, cy, r)
+    def area(self):
+        return sp.pi * self.r**2
+    def circumference(self):
+        return 2*sp.pi*self.r
+    def inside(self,ix,iy):
+        dx = abs(ix - self.cx)
+        dy = abs(iy - self.cy)
+        distance = 0
+
+        distance = sp.sqrt(dx**2 + dy**2)
+        if distance <= self.r:
+            print("inside")
+        else :
+            print("not in the circle")
+
+circle1 = circle(r = 10)
+print(circle1.area())
+circle1.inside(ix = 4 , iy = 4 )
